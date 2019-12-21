cf3a50c5edce8f641d0750889b2fc877034c58e1
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-12-20 18:57:28 -0700

    Adding the setup.py

--- matcher.py
+++ matcher.py
@@ -1,3 +1,9 @@
+import sys
+
+__author__ = 'Alfonso Villalobos'
+__version__ = '0.1'
+__license__ = 'MIT'
+
 from bottle_router import router
 
 class Matcher(Router):
@@ -6,12 +12,13 @@ class Matcher(Router):
         '''
         Constructor function
         '''
-        pass
+        self.router = Router()
 
     def excect_route(self, websockets, path):
         '''
         Function dedicated to return the function that the path wants binding
         the websockets for fourther use of it
         '''
+        self.socket = websockets
         route_tuple = self.match(path)
         route_tuple[0].callback(route_tuple[1])
